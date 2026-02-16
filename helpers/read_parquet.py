c7058b35437d5790625523c482cadca22312a46a
ChrisLam790 <christopherlam790@gmail.com>
2026-02-16 15:50:45 -0500

    adjust helper

--- helpers/read_parquet.py
+++ helpers/read_parquet.py
@@ -6,9 +6,10 @@ Helper for reading parquete files
 import pandas as pd
 from pathlib import Path
 
+import os
 
 
-def main(parquete_path: str) -> None:
+def read_one_parquet(parquete_path: str) -> None:
     
     try:
         df = pd.read_parquet(parquete_path)
@@ -19,7 +20,25 @@ def main(parquete_path: str) -> None:
 
 
 
+def read_all_parquets_in_folder(folder_path: str) -> None:
+    
+    try:
+    
+        files = []
+    
+        for filename in os.listdir(folder_path):
+            full_path = os.path.join(folder_path, filename) # Get the full path
+            if os.path.isfile(full_path) and filename not in files: # Check if it is a file
+                #print(filename)
+                files.append(filename)
+            else:
+                print("ERR; File duplicate")
+    
+    except:
+        raise Exception("Err in reading fodler")
+
+
 
 if __name__ == "__main__":
-    main("fixed_file.parquet")
-    main("data/silver/intraday_prices/TOS Kaggle data week ending 2025 08 08_cleaning.parquet")
+
+    read_all_parquets_in_folder("data/silver/normalize")
